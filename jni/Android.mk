25aed4863acd62f8b90be89b67d3c510cfc9239d
kodjo baah <kodjo_baah@hotmail.com>
2014-06-17 15:39:52 +0100

    fixed android.mk so that it works on mac

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,15 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+OPENCV_SDK := /Users/kodjobaah/software/opencv/OpenCV-2.4.9-android-sdk
 include $(CLEAR_VARS)
 
 # OpenCV
 #OPENCV_CAMERA_MODULES:=on
 #OPENCV_INSTALL_MODULES:=on
-include C:\Users\whatamidoing\OpenCV-2.4.9-android-sdk\sdk\native\jni\OpenCV.mk
+include $(OPENCV_SDK)/sdk/native/jni/OpenCV.mk
 
 # ZeroMQ
 include $(CLEAR_VARS)
 
-include  C:\Users\whatamidoing\hey\whatamidoingffmpeg\jni\zeromq\Android.mk
+include  zeromq/Android.mk
 
 
 include $(CLEAR_VARS)
-include  C:\Users\whatamidoing\hey\whatamidoingffmpeg\jni\jzmq\Android.mk
+include  jzmq/Android.mk
